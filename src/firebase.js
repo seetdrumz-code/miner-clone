33d802181a9c2dda420efa8b931a2a4f39fbaff3
Your Name <you@example.com>
2026-05-28 01:28:47 +0100

    Fixed Firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -3,19 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  
   apiKey: "AIzaSyDhKtij3t7RAUJnJtl48GFDw0UY73v5CUA",
-  
   authDomain: "minerclone.firebaseapp.com",
-  
   projectId: "minerclone",
-  
   storageBucket: "minerclone.firebasestorage.app",
-  
   messagingSenderId: "778073921350",
-  
   appId: "1:778073921350:web:379621163241cabcdbb747"
-  
 };
 
 const app = initializeApp(firebaseConfig);
